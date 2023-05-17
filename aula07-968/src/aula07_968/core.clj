56f03666601f51a183d0682fd60bf194803d42c8
ramoneees <ramonp.rios@gmail.com>
2023-05-17 19:58:19 +0100

    aula 07 968

---
+++ aula07-968/src/aula07_968/core.clj
@@ -0,0 +1,17 @@
+(ns aula07-968.core)
+
+
+(def v []) ;; constante
+
+
+(defn processa-csv1 [csv1]
+  ;;todo
+  )
+
+(defn adiciona-valor-vetor [valor]
+  (let [b []
+        b (conj b 2)]
+    (conj b valor)
+    b))
+
+
